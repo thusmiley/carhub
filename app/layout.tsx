cf50889c9761d33030aa6dd1d6bc0a913af66d21
Thu Dang <>
2023-06-27 21:03:45 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Footer, Navbar } from "@/components";
+import "./globals.css";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
